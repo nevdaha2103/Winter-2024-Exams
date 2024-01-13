11dc57e54f76d7f20537fb5d5b3150c1ceb7c424
nevdaha2103 <vlabimir200628@gmail.com>
2024-01-14 01:03:29 +0200

    Also replaced the value of 'N' with 'repeat'

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,10 +1,10 @@
 // Return an array without duplicates
 
-duplicate = (text, N) => {
-  if (N <= 0) return [];
+duplicate = (text, repeat) => {
+  if (repeat <= 0) return [];
   else {
     duplicateText = [];
-    for (let i = 0; i < N; i++) {
+    for (let i = 0; i < repeat; i++) {
       duplicateText[i] = text;
     }
     return duplicateText;
