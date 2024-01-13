c853463a7bbd7499dbdcc3c3c254a4213c437a7a
nevdaha2103 <vlabimir200628@gmail.com>
2024-01-14 00:24:56 +0200

    Replace forEach with finding unique values and convert Set to Array

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -3,18 +3,10 @@
 "use strict";
 
 const distinct = (data) => {
-  const distinct = new Set();
-  let w = 0;
-  data.forEach((a) => {
-    if (distinctSet.has(a)) {
-      delete data[w];
-    } else {
-      distinctSet.add(a);
-    }
-    w++;
-  });
-  
-  return data.filter((x) => typeof x === "number");
+  const distinct = new Set(data);
+  const distinctSet = new Set(data);
+  const distinctArr = [...distinctSet];
+  return distinctArr;
 };
 
 module.exports = distinct;
