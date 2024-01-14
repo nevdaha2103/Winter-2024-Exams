a8d7d4d399b6fe7c2b5aaf767af1c2945619502c
nevdaha2103 <vlabimir200628@gmail.com>
2024-01-14 16:11:45 +0200

    update words.js Add 'use strict'

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,5 +1,42 @@
 // Count words in a string
 
-'use strict';
+"use strict";
 
-const countWords = (inputString) => inputString.trim().split(/\s+/).filter(word => word !== '').length;
+Words = function (s) {
+  number_of_words_in_s = 0;
+  flag = false;
+  for (c of s) {
+    if (!flag) {
+      if (c === " ") {
+        if (flag === true) {
+          flag = false;
+        } else {
+          flag = false;
+        }
+      } else {
+        if (flag === true) {
+          flag = true;
+        } else {
+          flag = true;
+        }
+        number_of_words_in_s++;
+      }
+    } else {
+      if (c === " ") {
+        if (flag === true) {
+          flag = false;
+        } else {
+          flag = false;
+        }
+      } else {
+        if (flag === true) {
+          flag = true;
+        } else {
+          flag = true;
+        }
+      }
+    }
+  }
+  return number_of_words_in_s;
+};
+module.exports = Words;
