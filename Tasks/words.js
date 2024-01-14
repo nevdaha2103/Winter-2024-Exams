a1b44aa9e8b3c15caebc7866d58614c97caa2d5d
nevdaha2103 <vlabimir200628@gmail.com>
2024-01-14 14:29:47 +0200

    Remove unnecessary flags

--- Tasks/words.js
+++ Tasks/words.js
@@ -2,43 +2,4 @@
 
 'use strict';
 
-const countWords = (inputString) => {
-  const wordsArray = inputString.trim().split(/\s+/);
-  let numberOfWords = 0;
-  flag = false;
-  for (c of inputString) {
-    if (!flag) {
-      if (c === ' ') {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
-        numberOfWords++;
-      }
-    } else {
-      if (c === ' ') {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
-      }
-    }
-  }
-  return numberOfWords;
-};
-
-module.exports = countWords;
+const countWords = (inputString) => inputString.trim().split(/\s+/).filter(word => word !== '').length;
